907340bf52c5a56cb306e31825b29088a2e4d69f
Naman <146616222+Namangthub@users.noreply.github.com>
2025-11-06 12:48:39 +0530

    fixed error in package json

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import HealthcareDashboard from './components/HealthcareDashboard.jsx';
+import HealthcareDashboard from './components/HealthCareDashboard.jsx';
 import axios from "axios"
 function App() {
   const [count, setCount] = useState(0)
